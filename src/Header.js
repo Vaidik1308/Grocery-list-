3d58f87bced4e1a02641acdf859feb4a23705dcb
Vaidik Singh Nirwan <vaidiksinghnirwan.com>
2022-11-21 20:42:17 +0530

    removes comments from the Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,8 @@
 import React from 'react'
 
-const Header = ({title}) => {   //destructuring
-    // const name = 'vaidik'
-    // const headerStyle = {backgroundColor:'royalblue', color: 'yellow'};
+const Header = ({title}) => { 
   return (
     <header>
-        {/* <p>{name}</p> */}
-        {/* <h1>Groceries list</h1> */}
         <h1>{title}</h1>
 
     </header>

52aae1bf30d0df07dad97c4b392cdb8f9c6c41e6
Vaidik Singh Nirwan <vaidiksinghnirwan.com>
2022-11-23 10:56:25 +0530

    App,js is working properly after assigning a initial entry of list

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-
-import { useState } from 'react';
 import Header from './Header';
+import SearchItem from './SearchItem';
+import AddItem from './AddItems';
 import Content from './Content';
 import Footer from './Footer';
-import AddItems from './AddItems';
-import SearchItem from './SearchItem';
+import { useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')));
-
-  const [newItem,setNewItem] = useState('');
-
-  const [search,setSearch] = useState('')
+  const [items, setItems] = useState([
+    {
+      item:"item1",
+      id: 1,
+      checked:false,
+    }
+    
+  ]);
+  const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
+  
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  }
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = 
-    {
-      id,
-      checked:false,
-      item,
-    }
-    const listItems = [...items,myNewItem];
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
     setAndSaveItems(listItems);
   }
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem('shoppingList',JSON.stringify(newItems))
-  }
-  
   const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? {...item,checked: !item.checked} : item);
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setAndSaveItems(listItems);
   }
 
@@ -39,13 +38,10 @@ function App() {
     const listItems = items.filter((item) => item.id !== id);
     setAndSaveItems(listItems);
   }
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!newItem) {
-      return;
-    }
-
+    if (!newItem) return;
     addItem(newItem);
     setNewItem('');
   }
@@ -53,20 +49,19 @@ function App() {
   return (
     <div className="App">
       <Header title="Grocery List" />
-      
-      <AddItems
-        newItem = {newItem}
-        setNewItem= {setNewItem}
-        handleSubmit = {handleSubmit}
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
       />
       <SearchItem
         search={search}
         setSearch={setSearch}
       />
-      <Content 
-        items = {items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck = {handleCheck}
-        handleDelete = {handleDelete} 
+      <Content
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
       />
       <Footer length={items.length} />
     </div>
